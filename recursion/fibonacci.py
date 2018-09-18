361c4b4c7073ddf37e52980e8c551f47ca5df774
Chanon Roy <chanonr@surveymonkey.com>
2018-09-18 10:19:10 -0400

    fibonacci o(1) space complexity

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -4,7 +4,7 @@ def fib_recursive(n):
     # Check if Base Case
     if n <= 2:
         return 1
-    
+
     return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 # print(fib_recursive(6))
@@ -26,11 +26,26 @@ def fib_dynamic(n, memo):
         memo[n] = fib_number
         return fib_number
 
-print(fib_dynamic(100, memoMap))
+# print(fib_dynamic(100, memoMap))
 
-def fib_iterative(n):
+
+def fib_bottom_up(n):
     # Linear Time O(N), Constant Space O(1)
 
-    # TODO
+    if n == 0 or n == 1:
+        return n
+
+    twoBehind = 0
+    oneBehind = 1
+    fib = 0
+
+    for i in range(1, n + 1):
+        fib = oneBehind + twoBehind
+        twoBehind = oneBehind
+        oneBehind = fib
+
+    return fib
 
 
+result = fib_bottom_up(1000)
+print(result)
